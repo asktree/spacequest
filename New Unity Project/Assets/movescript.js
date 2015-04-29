d7be9305839f626d972a904d2e94886d87feef49
asockman <agrippakellum@gmail.com>
2015-04-29 01:09:29 -0500

    settings, movement, collision
    can move. disabled arrowkeys for getaxis. disabled gravity

--- New Unity Project/Assets/movescript.js
+++ New Unity Project/Assets/movescript.js
@@ -1,43 +1,18 @@
 ﻿#pragma strict
-public var movingto = Vector2(0,0);
-public var domove = false;
-function Start () {
-	domove = false;
+
+var speed : float = 1.0;
+var rb : Rigidbody2D;
+
+function Start() {
+	rb = GetComponent.<Rigidbody2D>();
+	rb.fixedAngle = true;
 }
 
-function Update () {
-	var tilesize : float = 1;
-	if(Input.GetMouseButton(0) && domove == false) {
 
-		var tilex = Mathf.Floor(Camera.main.ScreenToWorldPoint(Input.mousePosition).x / tilesize);
-		var tiley = Mathf.Floor(Camera.main.ScreenToWorldPoint(Input.mousePosition).y / tilesize);
-		
-		var playertilex = Mathf.Floor(transform.position.x / tilesize);
-		var playertiley = Mathf.Floor(transform.position.y / tilesize);
-		
-		if(Mathf.Abs(playertilex - tilex)+Mathf.Abs(playertiley-tiley) == 1)
-		{
-			movingto = Vector2(tilex,tiley);
-			domove = true;
-		}
-		
-		Debug.Log(tilex + " " + playertilex);
-	}
-	
-	var movespeed : float = 2.0;
-	
-	if(!domove){
-	}
-	else if(Vector2.Distance((movingto*tilesize)+Vector2(tilesize/2,tilesize/2),Vector2(transform.position.x,transform.position.y)) < movespeed*Time.deltaTime) {
-		Debug.Log(transform.position.x);
-		transform.position = ((movingto*tilesize)+Vector2(tilesize/2,tilesize/2));
-		Debug.Log(transform.position.x);
-		domove = false;
-	}
-	else {
-		Debug.Log((movingto*tilesize)+Vector2(tilesize/2,tilesize/2));
-		var directionvector : Vector2 = (movingto*tilesize)+Vector2(tilesize/2,tilesize/2)-Vector2(transform.position.x,transform.position.y);
-		directionvector.Normalize();
-		transform.Translate(movespeed*directionvector*Time.deltaTime);
-	}
+function Update () {
+	var move = Vector2.zero;
+	move += Input.GetAxisRaw("Horizontal")*Vector2.right;
+	move += Input.GetAxisRaw("Vertical")*Vector2.up;
+	Vector3.Normalize(move);
+	rb.velocity= move*speed;
 }
