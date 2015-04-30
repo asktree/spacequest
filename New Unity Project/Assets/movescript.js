4981e5df225860ffec60d132536a49a769a5abeb
asockman <agrippakellum@gmail.com>
2015-04-30 00:49:41 -0500

    bungus

--- New Unity Project/Assets/movescript.js
+++ New Unity Project/Assets/movescript.js
@@ -13,6 +13,6 @@ function Update () {
 	var move = Vector2.zero;
 	move += Input.GetAxisRaw("Horizontal")*Vector2.right;
 	move += Input.GetAxisRaw("Vertical")*Vector2.up;
-	Vector3.Normalize(move);
+	move = Vector3.Normalize(move);
 	rb.velocity= move*speed;
 }
